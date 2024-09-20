bc0d0413e6c4c4d17fcfa9b83c5cb9924870b98e
Nigmatjonov Muhammadjon <148071379+zero1max@users.noreply.github.com>
2024-09-20 18:33:29 +0500

    First Commit

---
+++ task3.py
@@ -0,0 +1,14 @@
+# Task 3
+
+while True:
+    baho = int(input("Baxo kiriting: "))
+    if baho>0 and baho<=54:
+        print(2)
+    elif baho>=55 and baho<=70:
+        print(3)
+    elif baho>=71 and baho<=84:
+        print(4)
+    elif baho>=85 and baho<=100:
+        print(5)
+    else:
+        print("6 baho yo'q")
