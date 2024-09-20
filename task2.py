284026f76a5385794e9d0c2bf3c63e9193b22373
Nigmatjonov Muhammadjon <148071379+zero1max@users.noreply.github.com>
2024-09-20 18:32:27 +0500

    First Commit

---
+++ task2.py
@@ -0,0 +1,12 @@
+# Task 2
+
+while True:
+    fasl = int(input("Fasl raqamini kiriting: "))
+    if fasl == 1 or fasl == 2 or fasl == 12:
+        print('Qish')
+    if fasl == 3 or fasl == 4 or fasl == 5:
+        print('Bahor')
+    if fasl == 6 or fasl == 7 or fasl == 8:
+        print('Yoz')
+    if fasl == 9 or fasl == 10 or fasl == 11:
+        print('Kuz')
